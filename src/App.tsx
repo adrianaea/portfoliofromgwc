a6e7dd632ee8624bb73650e6b0093ae53dbb6dee
adrianaea <137739910+adrianaea@users.noreply.github.com>
2026-03-12 10:53:41 -0400

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <BrowserRouter>
+        <BrowserRouter basename="/portfoliofromgwc">
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
